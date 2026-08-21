a7cff0cdc9b7e405d3f9ddf20b8e4767d3012c16
peyrin <peyrin@peyrin>
2026-08-20 23:16:54 -0700

    feat: stolen dark mode switcher

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ban-lily-fish-staff'
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:////data/app.db'
     #    'sqlite:///' + os.path.join(basedir, 'data/app.db')
     #SQLALCHEMY_DATABASE_URI = 'sqlite:////home/peyrin/Downloads/showdown/data/app.db'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////data/app.db'
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:////data/app.db'
