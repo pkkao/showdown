707625872b628cc2163b2b4efe60885123f3e3fa
peyrin <peyrin@peyrin>
2026-08-20 22:56:42 -0700

    fix: db path for gcloud

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ban-lily-fish-staff'
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
     #    'sqlite:///' + os.path.join(basedir, 'data/app.db')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////home/peyrin/Downloads/showdown/data/app.db'
-    #SQLALCHEMY_DATABASE_URI = 'sqlite:////data/app.db'
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:////home/peyrin/Downloads/showdown/data/app.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////data/app.db'
