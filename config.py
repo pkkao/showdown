84e9bad787f5c950bcd7d62d4f06ed55980756a8
peyrin <peyrin@peyrin>
2026-08-20 20:22:50 -0700

    fix: try to get gcloud to understand db location

--- config.py
+++ config.py
@@ -4,5 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ban-lily-fish-staff'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    #    'sqlite:///' + os.path.join(basedir, 'data/app.db')
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:////home/peyrin/Downloads/showdown/data/app.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////data/app.db'
